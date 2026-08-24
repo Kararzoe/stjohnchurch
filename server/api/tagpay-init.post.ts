c50113aa041b7f906d9b6483650978541e4b9620
Kararzoe <kararzoe@gmail.com>
2026-08-24 23:06:02 +0100

    TagPay: try /v1/checkout/sessions

--- server/api/tagpay-init.post.ts
+++ server/api/tagpay-init.post.ts
@@ -7,7 +7,7 @@ export default defineEventHandler(async (event) => {
   }
 
   try {
-    const res = await $fetch<any>('https://gwt.tagpay.ng/checkout/sessions', {
+    const res = await $fetch<any>('https://gwt.tagpay.ng/v1/checkout/sessions', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${config.tagpaySecretKey}`,
